da512318f5836aee86dfb3b06b560324c8051885
ChenYing Kuo <evshary@gmail.com>
2020-11-24 18:58:02 +0800

    Able to take snapshot on NeuronBot.

--- src/napp_inspection/launch/neuronbot_inspection.launch.py
+++ src/napp_inspection/launch/neuronbot_inspection.launch.py
@@ -6,17 +6,21 @@ from launch.actions import (IncludeLaunchDescription, GroupAction)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions.node import Node
+from pathlib import Path
 
 import pdb
 def generate_launch_description():
     # Path
     nb2_launch_dir = os.path.join(get_package_share_directory('neuronbot2_bringup'), 'launch')
     nb2nav_launch_dir = os.path.join(get_package_share_directory('neuronbot2_nav'), 'launch')
-    nb2nav_map_dir = os.path.join(get_package_share_directory('neuronbot2_nav'), 'map')
+    your_map_path = str(Path.home())+'/neuron_app_slam/yourmap.yaml'
+
+    if not os.path.isfile(your_map_path):
+        raise RuntimeError('\x1b[0;37;41m'+'Please run Neuron APP SLAM to create your own map first.'+'\x1b[0m')
 
     # Parameters
     open_rviz = LaunchConfiguration('open_rviz', default='True')
-    map_path = LaunchConfiguration('map', default=nb2nav_map_dir+'/mememan.yaml')
+    map_path = LaunchConfiguration('map', default=your_map_path)
     use_camera = LaunchConfiguration('use_camera', default='top')
 
     neuron_app_bringup = GroupAction([
@@ -33,7 +37,10 @@ def generate_launch_description():
     image_saver = Node(
         package='image_view',
         executable='image_saver',
-        parameters=[{'image': 'ros2_openvino_toolkit/image_rviz'}],
+        remappings=[('image', 'color/image_raw')],
+        parameters=[{
+            'save_all_image': False
+            }],
         output='screen'
     )
 
